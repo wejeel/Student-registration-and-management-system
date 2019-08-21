f0bed9bf36b3ca462e6571dd194b9ddb457669ef
Weje Praise <wejepraise@yahoo.com>
2019-08-21 09:31:10 +0100

    initialize

---
+++ src/student/registration/and/management/system/STUDENTREGISTRATIONANDMANAGEMENTSYSTEM.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package student.registration.and.management.system;
+
+/**
+ *
+ * @author WEJE
+ */
+import javax.swing.*;
+public class STUDENTREGISTRATIONANDMANAGEMENTSYSTEM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
